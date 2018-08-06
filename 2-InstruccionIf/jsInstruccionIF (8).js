0603431f0c91b2549c22de573818bd8213d53620
NoeliaLuna <40829921+NoeliaLuna@users.noreply.github.com>
2018-08-06 01:10:27 -0300

    Update jsInstruccionIF (8).js

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,10 @@
 function mostrar()
 {
-//tomo la edad  
+	var valEdad = parseInt(document.getElementById("edad").value);
+	var valEstadoCivil = document.getElementById("estadoCivil").value;
 
-	
-
-
+	if (valEdad >= 18 && valEstadoCivil == "Soltero")
+	{
+		alert("Es soltero y no es menor.");
+	}
 }//FIN DE LA FUNCIÓN
