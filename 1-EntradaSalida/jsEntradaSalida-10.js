5f1f5c72ea8233b2559e66e7354a870e3729e3c1
NoeliaLuna <40829921+NoeliaLuna@users.noreply.github.com>
2018-08-06 01:03:30 -0300

    Update jsEntradaSalida-10.js

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,7 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var numImporte = parseInt(document.getElementById("importe").value);
+	var calcDescuento = numImporte - ((numImporte * 25)/100);
+	document.getElementById("resultado").value = calcDescuento;	
 }
