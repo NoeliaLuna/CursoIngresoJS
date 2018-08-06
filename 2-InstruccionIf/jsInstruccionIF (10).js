637b5b12417e99de1873045f2a381e07cbe8c1f2
NoeliaLuna <40829921+NoeliaLuna@users.noreply.github.com>
2018-08-06 01:08:15 -0300

    Update jsInstruccionIF (10).js

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
+	var numRandom = Math.floor(Math.random()*10)+1;
+
+	if (numRandom == 9 || numRandom == 10)
+	{
+		alert("EXCELENTE, te sacaste un " + numRandom);
+	}
+	else if (numRandom <= 8 && numRandom >=4)
+	{
+		alert("APROBO, te sacaste un " + numRandom);
+	}
+	else if (numRandom <4 && numRandom >=0) 
+	{	
+		alert("Tu nota fue un "+ numRandom +"... Mas suerte para proxima");
+	}
 
 }//FIN DE LA FUNCIÓN
