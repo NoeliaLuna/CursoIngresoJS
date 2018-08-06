e1a9cb3cb8af31e10036f7f5bcf85c2e50988a9c
NoeliaLuna <40829921+NoeliaLuna@users.noreply.github.com>
2018-08-06 01:09:07 -0300

    Update jsInstruccionIF (4).js

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
-//tomo la edad  
+	var valEdad = parseInt(document.getElementById("edad").value);  
+	if (valEdad >= 18)
+	{
+		alert("Eres mayor de edad.");
+	}
+	else
+	{
+		if (valEdad >=12 && valEdad<=17)
+		{
+				alert("Eres puberto.");
+		}
+		else
+		{
+			alert("chichon de piso");
+		}
+	}  
 
 
 
