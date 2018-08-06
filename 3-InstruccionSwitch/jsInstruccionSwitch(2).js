ddda1cf4bae2a50626bff574a425e09a02a00637
NoeliaLuna <40829921+NoeliaLuna@users.noreply.github.com>
2018-08-06 01:12:06 -0300

    Update jsInstruccionSwitch(2).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,26 @@
 function mostrar()
-{
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+{ 
+	var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+	if (mesDelAño != "Febrero")
+	{
+		mesDelAño = "Otros";
+	}
+
+	switch (mesDelAño)
+	{
+		case "Febrero":
+			{
+				alert("Este mes no tiene más de 29 días.");
+				break;
+			}
+		case "Otros":
+			{
+				alert("Este mes tiene 30 o más días");
+				break;
+			}
+	}
+
 
 
 
