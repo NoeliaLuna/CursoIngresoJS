40247def5b29cedef254ba4b94cde81f93cb8d49
NoeliaLuna <40829921+NoeliaLuna@users.noreply.github.com>
2018-08-06 01:12:46 -0300

    Update jsInstruccionSwitch(4).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -3,7 +3,32 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+	if (mesDelAño=="Abril"||mesDelAño=="Junio"||mesDelAño=="Septiembre"||mesDelAño=="Noviembre")
+	{
+		mesDelAño = "dias30";//30 = Abril Junio Septiembre Noviembre
+	}
+	else if (mesDelAño=="Enero"||mesDelAño=="Marzo"||mesDelAño=="Mayo"||mesDelAño=="Julio"||mesDelAño=="Agosto"||mesDelAño=="Octubre"||mesDelAño=="Diciembre") 
+	{
+		mesDelAño = "dias31";//31 =  Enero Marzo Mayo Julio Agosto Octubre Diciembre
+	}
+	switch (mesDelAño)
+	{
+		case "Febrero":
+			{
+				alert("Este mes tiene 28 dias");
+				break;
+			}
+		case "dias30":
+			{
+				alert("Este mes tiene 30 dias");
+				break;
+			}
+		case "dias31":
+			{
+				alert("Este mes tiene 31 dias");
+				break;
+			}
+	}
 	
 	
 
