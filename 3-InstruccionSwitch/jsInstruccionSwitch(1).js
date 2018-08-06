4e0e1d83254deabdad8410d306105f12661ba6c7
NoeliaLuna <40829921+NoeliaLuna@users.noreply.github.com>
2018-08-06 01:11:49 -0300

    Update jsInstruccionSwitch(1).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,29 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
-
-
+	switch (mesDelAño)
+	{
+		case "Enero":
+			{
+				alert("Que comiences bien el año!!!.");
+				break;
+			}
+		case "Marzo":
+			{
+				alert("a clases!!!.");
+				break;
+			}
+		case "Julio":
+			{
+				alert("se vienen las vacaciones!!!.");
+				break;
+			}
+		case "Diciembre":
+			{
+				alert("Felices fiesta!!!.");
+				break;
+			}
+	}
 
 }//FIN DE LA FUNCIÓN
