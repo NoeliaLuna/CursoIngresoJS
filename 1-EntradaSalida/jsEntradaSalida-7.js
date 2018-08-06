71e5737cd2a55bdd0d3ae80a964793df7a1a09c1
NoeliaLuna <40829921+NoeliaLuna@users.noreply.github.com>
2018-08-06 01:05:20 -0300

    Update jsEntradaSalida-7.js

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,32 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
-}
+	var numero1 = document.getElementById("numeroUno").value;
+	var numero2 = document.getElementById("numeroDos").value;
 
+	var resultado = parseInt(numero1)+parseInt(numero2);
+	alert("La suma es "+ resultado);
+}
 function restar()
 {
-	
+	var nuemero1 = document.getElementById("numeroUno").value;
+	var nuemero2 = document.getElementById("numeroDos").value;
+	var resultado = parseInt(nuemero1) - parseInt(nuemero2);
+	alert("La resta es "+ resultado);
 }
 
 function multiplicar()
 { 
-	
+	var nuemero1 = document.getElementById("numeroUno").value;
+	var nuemero2 = document.getElementById("numeroDos").value;
+	var resultado = parseInt(nuemero1) * parseInt(nuemero2);
+	alert("La multiplicación es "+ resultado);
 }
 
 function dividir()
 {
-	
+	var nuemero1 = document.getElementById("numeroUno").value;
+	var nuemero2 = document.getElementById("numeroDos").value;
+	var resultado = parseInt(nuemero1) / parseInt(nuemero2);
+	alert("La división es "+ resultado);
 }
-
