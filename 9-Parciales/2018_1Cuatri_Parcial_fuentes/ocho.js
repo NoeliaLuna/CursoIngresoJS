43268d629f4128fc0b82a5ec662e146475b2c968
NoeliaLuna <40829921+NoeliaLuna@users.noreply.github.com>
2018-08-07 13:05:03 -0300

    Update ocho.js

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,29 @@
 function mostrar()
 {
+	var valLetra, valNumero;
+	var valCont=0;
+	var valContinuar = "s";
+
+	while(valContinuar = "s")
+	{
+		valNumero=parseInt(prompt("Ingrese un numero valido entre -100 y 100","-50"));
+		valLetra=prompt("Ingrese una Letra","f");
+
+		if(valNumero>=-100 && valNumero <=100)
+		{
+			
+
+
+
+			
+			alert("Queres contuniar? (s = si, n = no)","s o n");
+		}
+		else 
+		{
+			alert("Fijate lo que ingresas.");
+			alert("Queres contuniar? (s = si, n = no)","s o n");
+		}
+	}
 
 }
+
